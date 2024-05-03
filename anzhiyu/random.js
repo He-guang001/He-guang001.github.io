4a9f8ad136d1b2610418fe71de82463f012ca668
He-guang001 <3491487309@qq.com>
2024-05-03 21:49:13 +0800

    Site updated: 2024-05-03 21:49:12

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2024/04/30/hello-world/","2024/05/02/记2024五一假期（一）/"];function toRandomPost(){
+var posts=["2024/04/30/hello-world/","2024/05/02/记2024五一假期（一）/","2024/05/03/测试/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
