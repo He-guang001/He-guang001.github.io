84f006f467ed96f8a5207d0b1179517903c8dd48
He-guang001 <3491487309@qq.com>
2025-03-02 23:38:53 +0800

    Site updated: 2025-03-02 23:38:52

--- anzhiyu/random.js
+++
@@ -1,3 +0,0 @@
-var posts=["2024/04/30/hello-world/","2024/05/30/2024.5.10/","2024/06/03/2024-6-3/"];function toRandomPost(){
-    pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
-  };
